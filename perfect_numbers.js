4d825c9bef880ff76a29e2df02e3fc5ecd2506b7
Akin Oluyele <waleander@gmail.com>
2016-12-01 17:12:52 +0100

    beautify and improve ES6 syntax

--- perfect_numbers.js
+++ perfect_numbers.js
@@ -1,20 +1,19 @@
-function PerfectNumber(number){
-  let sum = 0;
-  let halved = number/2;
-  let result = false;
+function PerfectNumber(number) {
+    let sum = 0;
+    let halved = number / 2;
+    let result = false;
 
-  for(i = 1; i <= halved; i++){
-    if(number % i === 0){
-      sum += i;
+    for (i = 1; i <= halved; i++) {
+        if (number % i === 0) {
+            sum += i;
+        }
     }
-  }
 
-  if(sum === number){
-    result = true;
-  }
-  else  {
-    result = false;
-  }
+    if (sum === number) {
+        result = true;
+    } else {
+        result = false;
+    }
 
-  return result;
+    return result;
 }
